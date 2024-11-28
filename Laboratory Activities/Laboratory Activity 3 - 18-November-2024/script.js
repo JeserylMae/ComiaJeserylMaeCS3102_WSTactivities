375c7f4578b2a894db6cebbedc094efd090db3b1
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-11-28 11:46:58 +0800

    added lab 4

--- Laboratory Activities/Laboratory Activity 3 - 18-November-2024/script.js
+++ Laboratory Activities/Laboratory Activity 3 - 18-November-2024/script.js
@@ -7,7 +7,7 @@ textInput.addEventListener('keydown', () => {
     let currentInput = textInput.value;
     
     let wordCount = currentInput.split(/\s+/).filter(word => word.length > 0).length;
-    let sentenceCount = currentInput.split(/\./).filter(word => word.length > 0).length;
+    let sentenceCount = currentInput.split(/[.!]/).filter(word => word.length > 0).length;
 
     wordCountHolder.textContent = `Words: ${wordCount}`
     sentenceCountHolder.textContent = `Sentences: ${sentenceCount}`
